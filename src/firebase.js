6417a1d6b93bc2b2955e1598351446976048ab41
Aditya0257 <adityasinghthakur.0257@gmail.com>
2023-08-03 15:10:10 +0530

    updated firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvmG3HoFJ3cU1Kh23NR5EUS6zcsLeBjuY",
-  authDomain: "react-firebase-homepage.firebaseapp.com",
-  projectId: "react-firebase-homepage",
-  storageBucket: "react-firebase-homepage.appspot.com",
-  messagingSenderId: "189558888241",
-  appId: "1:189558888241:web:fb42589689060fb0cfc210",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
