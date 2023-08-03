939ef698ebc0f79a8f3c1853c746f66c46002c0a
Aditya0257 <adityasinghthakur.0257@gmail.com>
2023-08-03 15:04:16 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWMX6hZ_EIi2Ds272Jqel1yA0PIDMy_sQ",
-  authDomain: "linkedin-clone-27716.firebaseapp.com",
-  projectId: "linkedin-clone-27716",
-  storageBucket: "linkedin-clone-27716.appspot.com",
-  messagingSenderId: "741345535982",
-  appId: "1:741345535982:web:9a9824b590dfef5becd0ff"
+  apiKey: "AIzaSyAvmG3HoFJ3cU1Kh23NR5EUS6zcsLeBjuY",
+  authDomain: "react-firebase-homepage.firebaseapp.com",
+  projectId: "react-firebase-homepage",
+  storageBucket: "react-firebase-homepage.appspot.com",
+  messagingSenderId: "189558888241",
+  appId: "1:189558888241:web:fb42589689060fb0cfc210",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
